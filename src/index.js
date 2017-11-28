632ea2c8980ac30ce180f6b3f13e7c93cd035eb5
gsat-technology <gsat.technology@gmail.com>
2017-11-28 19:41:16 +1100

    removed redux and updated cicd

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/App';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'))
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,39 +1,35 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import _$ from 'jquery'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import ReactTestUtils from 'react-dom/test-utils'
-import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from '../src/reducers'
+import jsdom from 'jsdom'
+import chai, { expect } from 'chai'
+import chaiJquery from 'chai-jquery'
+import { createStore } from 'redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+global.window = global.document.defaultView
+global.navigator = global.window.navigator
+const $ = _$(window)
 
-chaiJquery(chai, chai.util, $);
+chaiJquery(chai, chai.util, $)
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
   const componentInstance =  ReactTestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <MuiThemeProvider>
-        <ComponentClass {...props} />
-      </MuiThemeProvider>
-    </Provider>
+    <MuiThemeProvider>
+      <ComponentClass {...props} />
+    </MuiThemeProvider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  return $(ReactDOM.findDOMNode(componentInstance))
 }
 
 $.fn.simulate = function(eventName, value) {
   if (value) {
-    this.val(value);
+    this.val(value)
   }
-  ReactTestUtils.Simulate[eventName](this[0]);
-};
+  ReactTestUtils.Simulate[eventName](this[0])
+}
 
-export {renderComponent, expect};
+export {renderComponent, expect}
