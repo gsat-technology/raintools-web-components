058f9ea59f4891a76c7b4f6d9f9f51dfc5ca13fb
gsat-technology <gsat.technology@gmail.com>
2018-01-07 13:16:50 +1100

    solve webpack issue in build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: [__dirname + '/src/index.js'],
   output: {
     path: __dirname + '/lib',
     publicPath: '/',
@@ -9,7 +9,7 @@ module.exports = {
     loaders: [
       {
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-1']
         }
@@ -17,7 +17,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
